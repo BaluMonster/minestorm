fc2e5cfa7fe0ab0c501a88fb71a458686c42772b
Pietro Albini <pietro@pietroalbini.io>
2014-08-23 20:05:24 +0200

    Added event triggering system (with unit test)

--- minestorm/test/common/events.py
+++ minestorm/test/common/events.py
@@ -27,7 +27,7 @@ class EventsTestCase( unittest.TestCase ):
         event = self.events.get('test') # Get the event
         # Try to listen correctly
         self.events.listen('test', subscriber, 10)
-        self.assertTrue( subscriber in event._subscribers[10] )
+        self.assertTrue( subscriber in event._listeners[10] )
         self.events.unlisten('test', subscriber, 10) # Unlisten afterr to prevents errors in the future
         # Try to pass an invalid event name
         with self.assertRaises( ValueError ):
@@ -50,12 +50,54 @@ class EventsTestCase( unittest.TestCase ):
         event = self.events.get('test')
         # First try to unlisten it properly
         self.events.listen('test', subscriber, 20)
-        self.assertTrue( subscriber in event._subscribers[20] )
+        self.assertTrue( subscriber in event._listeners[20] )
         self.events.unlisten('test', subscriber, 20)
-        self.assertFalse( subscriber in event._subscribers[20] )
+        self.assertFalse( subscriber in event._listeners[20] )
         # Next try to unlisten a subscriber which wasn't listening
         with self.assertRaises( ValueError ):
             self.events.unlisten('test', subscriber, 10)
         # Also try to unlisten from an event which doesn't exist
         with self.assertRaises( ValueError ):
             self.events.unlisten('test2', subscriber, 20)
+
+    def test_trigger_event(self):
+        """ Test the event trigger process """
+        # Setup things
+        self.events.create('test')
+        self.events.listen('test', ( lambda event: 'a' ), 10)
+        self.events.listen('test', ( lambda event: 'b' ), 20)
+        # Try to trigger the event
+        result = self.events.trigger('test')
+        self.assertEqual(result.returns, ['b', 'a'])
+
+    def test_trigger_event_with_data(self):
+        """ Test trigger event with some data """
+        # Setup things
+        self.events.create('test')
+        self.events.listen('test', ( lambda event: event.data ), 10)
+        # Try to trigger the event
+        result = self.events.trigger('test', {'a': 'b'})
+        self.assertEqual(result.returns, [{'a': 'b'}])
+
+    def test_block_trigger_process(self):
+        """ Test blocking the trigger process """
+        self.events.create('test')
+        self.events.listen('test', ( lambda event: 'a' ), 10)
+        self.events.listen('test', ( lambda event: 'b' ), 20)
+        self.events.listen('test', ( lambda event: event.block() ), 15 )
+        # Try to trigger the event
+        result = self.events.trigger('test')
+        self.assertEqual(result.returns, ['b'])
+        self.assertTrue(result.blocked)
+
+    def test_retrigger_event(self):
+        """ Test re-triggering events """
+        self.events.create('test')
+        self.events.listen('test', ( lambda event: 'a' ), 10)
+        self.events.listen('test', ( lambda event: 'b' ), 20)
+        self.events.listen('test', ( lambda event: event.retrigger() if not event.retriggered else 'c' ), 15 )
+        # Try to trigger the event
+        result = self.events.trigger('test')
+        self.assertEqual(result.returns, ['b', 'b', 'c', 'a'])
+        self.assertTrue(result.retriggered)
+        self.assertFalse(result.blocked)
